2a68e5930c83a9e31d0e54ab9fe23aca9c5fcb77
dhampiree <dhampiree@gmail.com>
2016-06-13 07:12:39 +0300

    main.cpp
    exam cpp file

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vizhener 
+{
+public:
+  static string encodeStringWithKey(string stringToEncode, string key)
+  {
+    int stringLength = stringToEncode.length();
+    int keyLength = key.length();
+
+    char symbol;
+    char keySymbol;
+    bool isBig;
+    bool keySymbolIsBig;
+    char encryptedAlphaNumber;
+
+    for (int i = 0; i < stringLength; i++) 
+    {
+      symbol = stringToEncode[i];
+
+      if (isalpha(symbol) == false)
+        continue;
+
+      isBig = isupper(symbol);
+      keySymbol = key[i % keyLength];
+      keySymbolIsBig = isupper(keySymbol);
+
+      int numberOfAlpha = symbol - (isBig ? 'A' : 'a');
+      int numberOfKey   = keySymbol - (keySymbolIsBig ? 'A' : 'a');
+      symbol = (numberOfAlpha + numberOfKey) % 26;
+
+      stringToEncode[i] = symbol + (isBig ? 'A' : 'a');
+    }
+
+    return stringToEncode;
+  }
+};
+
+int main(int argc, char* argv[])
+{ 
+  if (argc != 2)
+    return 1;
+
+  string key = argv[1];
+  for (int i = 0; i < key.length(); i++){
+    if (!isalpha(key[i]))
+      return 1;
+  }
+
+  string stringToEncode;
+  cin >> stringToEncode; 
+
+  cout << Vizhener::encodeStringWithKey(stringToEncode, key);
+  
+  system("pause");
+  return 0;
+}
